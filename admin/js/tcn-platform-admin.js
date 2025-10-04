c6fd8ba0b6146b74045d58ca0e1627105fd61c7f
George Nicolaou <orionas.elite@gmail.com>
2025-10-04 16:51:19 +0300

    Improve activity log table responsiveness

--- admin/js/tcn-platform-admin.js
+++ admin/js/tcn-platform-admin.js
@@ -19,7 +19,13 @@
             order: [[0, 'desc']],
             autoWidth: false,
             language: localized,
-            dom: '<"tcn-platform-log-controls"lf>rt<"tcn-platform-log-footer"ip>'
+            dom: '<"tcn-platform-log-controls"lf>rt<"tcn-platform-log-footer"ip>',
+            columnDefs: [
+                { targets: 0, className: 'tcn-platform-log-col-time', width: '14%', responsivePriority: 1 },
+                { targets: 1, className: 'tcn-platform-log-col-source', width: '12%', responsivePriority: 4 },
+                { targets: 2, className: 'tcn-platform-log-col-message', width: '26%', responsivePriority: 2 },
+                { targets: 3, className: 'tcn-platform-log-col-details', responsivePriority: 3 }
+            ]
         };
 
         if (localized.lengthMenuAll) {
